ed237358df3d9adf58af05a5b0632d0132850e22
daichi-opsin <daichi.sato@ops-in.com>
2024-12-11 18:08:00 +0900

    /rails/info/notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # TODO: テスト
+  # FIXME: バグ
+  # OPTIMIZE: 最適化
 end
